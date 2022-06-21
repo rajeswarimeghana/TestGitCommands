f483a52056401ac0fdb423585b10ba88ac07a494
unknown <SESPL@NagaYogita.Symphonize.Com>
2022-06-21 10:52:22 +0530

    Testing 2 files

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+// main class
+class Arrays
+  {
+    // main method
+    public static void main(String args[])
+    {
+      // declare and initialize the array
+      // Syntax 1
+      // dt variable[size] = {v1, v2, ...};
+      String colors[] = {"Blue", "Red", "Black", "Orange", "Yellow"};
+
+      // for loop
+      for(int i = 0; i <= colors.length-1 ; i++)
+        {
+          System.out.print(colors[i]+ ", ");
+        }
+      System.out.println();
+
+      for(int i = colors.length - 1; i >= 0; i--) 
+        {
+          System.out.print(colors[i]+ ", ");
+        }
+    }
+  }
---
+++ SearchIndex.java
@@ -0,0 +1,39 @@
+// import scanner
+import java.util.Scanner;
+
+// main class
+class SearchIndex
+  {
+    // main method
+    public static void main(String args[])
+    {
+      // create an object for the scanner
+      Scanner sc = new Scanner(System.in);
+      // display the message
+      System.out.println("enter size of the array: ");
+      // store the length of the array;
+      int len = sc.nextInt();
+      // declare the array
+      String[] arr = new String[len];
+      // Store the elements in the array
+      
+      for(int i = 0; i < len; i++)
+        {
+         System.out.println("Enter the element : ");
+          arr[i] = sc.next();
+        }
+
+      // store the target index in a variable
+      System.out.println("enter search index position is: ");
+      int index = sc.nextInt();
+
+      // search the element using the target index value with all the array elements
+      for(int i = 0; i < len; i++)
+        {
+          if(index == i)
+          {
+            System.out.println("The search index position " +index+ " is found as : " +arr[i]);
+          }
+        }
+    }
+  }
